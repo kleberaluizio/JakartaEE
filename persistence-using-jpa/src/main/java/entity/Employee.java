147fd9ba90bb5070f66f82d4f052d6fb00249c78
kleberaluizio <kleberaluizio@gmail.com>
2023-09-29 11:35:27 -0300

    Configuring JDBC properties in persistence.xml

---
+++ persistence-using-jpa/src/main/java/entity/Employee.java
@@ -0,0 +1,40 @@
+package entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String fName;
+    private String lname;
+
+    public Employee(){}
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+}
